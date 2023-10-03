20be495434b1e0ed5c0cacf5395d967a078763ef
rydlande <rydlande@hotmail.com>
2023-10-03 16:33:00 +0200

    made function to auth user login

---
+++ js/login.js
@@ -0,0 +1,26 @@
+const url = "https://api.noroff.dev/api/v1/social/auth/login";
+const button = document.querySelector("button");
+const email = document.querySelector("#input-email");
+const password = document.querySelector("#input-password");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  login();
+});
+
+async function login() {
+  const user = {
+    email: email.value,
+    password: password.value,
+  };
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  const data = await res.json();
+  console.log(data);
+  localStorage.setItem("token", data.accessToken);
+}
