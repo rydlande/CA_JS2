4ed5b09fa5ae04387a8ad8befe6326739f638f21
rydlande <rydlande@hotmail.com>
2023-10-04 12:58:05 +0200

    login working correctly

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ const url = "https://api.noroff.dev/api/v1/social/auth/login";
 const button = document.querySelector("button");
 const email = document.querySelector("#input-email");
 const password = document.querySelector("#input-password");
+const response = document.querySelector("#response");
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
@@ -23,5 +24,15 @@ async function login() {
   const data = await res.json();
   console.log(data);
   localStorage.setItem("token", data.accessToken);
-  //   location.href = "../public/feed/";
+  if (data.status === "Bad Request") {
+    response.style.color = "red";
+    response.innerText = `Email or password is incorrect`;
+  } else if (data.status === "Unauthorized") {
+    response.style.color = "red";
+    response.innerText = `Email or password is incorrect`;
+  } else {
+    setTimeout(() => {
+      window.location.href = "../public/feed";
+    }, 1000);
+  }
 }
