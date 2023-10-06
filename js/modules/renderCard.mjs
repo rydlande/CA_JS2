96a1793dd0b11b633b34a325f3eeef35e950b4d3
griphaugland <griphaugland@gmail.com>
2023-10-06 18:21:51 +0200

    Added modal functionality to feed page

--- js/modules/renderCard.mjs
+++ js/modules/renderCard.mjs
@@ -2,13 +2,16 @@
 export default function renderCard(data){
     const card = document.createElement('div');
     card.classList.add('card');
+
     let {title, body, id, media, author, comments, reactions } = data;
+
     const cardContent = document.createElement('a');
     cardContent.classList.add('cardContent');
+
     const imageContainer = document.createElement('div');
     imageContainer.classList.add('imageContainer');
     const postImage = document.createElement('img');
-    const cardTop = document.createElement('div');
+
     const creator = document.createElement('a');
     creator.classList.add('creator');
     const creatorImage = document.createElement('img');
@@ -22,6 +25,8 @@ export default function renderCard(data){
         creatorImage.style.display = 'none';
        }
     creator.href= `../../public/profile/index.html?id=${author.id}`
+
+    const cardTop = document.createElement('div');
     cardTop.classList.add('cardTop');
     cardTop.append(creator)
     const cardTitle = document.createElement('h2');
@@ -35,10 +40,12 @@ export default function renderCard(data){
     cardContent.append(title, body, id,imageContainer)
     const cardBottom = document.createElement('div');
     cardBottom.classList.add('cardBottom');
-    const commentsShow = document.createElement('p');
+    const commentsShow = document.createElement('a');
+    commentsShow.href= `../../public/posts/index.html?id=${id}`
     commentsShow.innerText = `${comments.length} comments`
     commentsShow.classList.add('comments');
-    const reactionsShow = document.createElement('p');
+    const reactionsShow = document.createElement('a');
+    reactionsShow.href= `../../public/posts/index.html?id=${id}`
     reactionsShow.innerText = `${reactions.length} reactions`
     reactionsShow.classList.add('reactions');
     cardBottom.append(commentsShow, reactionsShow)
