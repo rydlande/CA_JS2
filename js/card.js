8c997d15a45365ac79ae45f2c1d30fde96e78185
griphaugland <griphaugland@gmail.com>
2023-10-15 20:03:46 +0200

    Finished search function

--- js/card.js
+++ js/card.js
@@ -16,6 +16,7 @@ import checkForErrors from './modules/checkForErrors.mjs'
  */
 
 async function getPosts(url){
+    root.innerHTML = '';
     let token = localStorage.getItem('token');
     const res = await fetch(url, {
         method: "GET",
@@ -25,22 +26,34 @@ async function getPosts(url){
           },
     })
     const data = await res.json()
-    const newData = filterPosts(data)
-    checkForErrors(newData)
-    console.log(newData)
-    let slicedData = newData.slice(startIndex, startIndex + postsPerPage);
+    checkForErrors(data)
+    let slicedData = data.slice(startIndex, startIndex + postsPerPage);
+    console.log(slicedData)
     slicedData.forEach(item => {
-        root.append(renderCard(item))
+            root.append(renderCard(item))
     });
-}
+    buttonMorePosts.style.display = data.length > startIndex + postsPerPage ? "flex" : "none";
+    buttonMorePosts.addEventListener("click", () => {
+        startIndex += postsPerPage;
+        let slicedData = data.slice(startIndex, startIndex + postsPerPage);
+        slicedData.forEach((item) => {
+          root.append(renderCard(item));
+        });
+        buttonMorePosts.style.display = data.length > startIndex + postsPerPage ? "flex" : "none";
+        console.log(slicedData)
+    });
+    if(root.innerHTML === ""){
+        root.innerHTML = '<h2 class="no-results">No results found</h2>'
+    }
+    }
 addEventListener('DOMContentLoaded', () => {
     getPosts(url)
 })
 
-buttonMorePosts.addEventListener("click", () => {
+/* buttonMorePosts.addEventListener("click", () => {
     startIndex += postsPerPage;
     getPosts(url);
-  });
+  }); */
 
 
 /* FILTER */
@@ -49,15 +62,12 @@ const filterOldest = document.querySelector('#filter-oldest');
 const filterFollowing = document.querySelector('#filter-following');
 
 filterNewest.addEventListener('click', () => {
-    root.innerHTML = '';
     getPosts(url + '&sortOrder=desc')
 });
 filterOldest.addEventListener('click', () => {
-    root.innerHTML = '';
     getPosts(url + '&sortOrder=asc')
 });
 filterFollowing.addEventListener('click', () => {
-    root.innerHTML = '';
     getPosts('https://api.noroff.dev/api/v1/social/posts/following?_author=true&_reactions=true&_comments=true')
 });
 
@@ -77,17 +87,13 @@ function filterPosts(data){
 const searchInput = document.querySelector('#search-input');
 const searchButton = document.querySelector('#search-input-button');
 
-searchButton.addEventListener('click', () => {
-    root.innerHTML = '';
-    getPosts(url + `&search=${searchInput.value}`)
-});
 
-searchInput.addEventListener('keyup', (e) => {
+searchButton.addEventListener('click', (e) => {
     e.preventDefault();
-    let postsArr = [];
-    async function searchPosts(){
+async function searchPosts(){
+        root.innerHTML = '';
         let token = localStorage.getItem('token');
-        const res = await fetch("https://api.noroff.dev/api/v1/social/posts?_tag=cute", {
+        const res = await fetch("https://api.noroff.dev/api/v1/social/posts/?_author=true&_reactions=true&_comments=true", {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
@@ -95,9 +101,49 @@ searchInput.addEventListener('keyup', (e) => {
               },
         })
         const data = await res.json()
-        const newData = filterPosts(data)
-        checkForErrors(newData)
-        console.log(newData)
+        checkForErrors(data)
+        filterSearch(data)
     }
     searchPosts()
 })
+function filterSearch(data){
+    let filteredArray = [];
+    let filterTitle  = data.filter(post => post.title.toLowerCase().includes(searchInput.value.toLowerCase()));
+    let filterBody = data.filter(post => post.body.toLowerCase().includes(searchInput.value.toLowerCase()));
+    let filterTag = data.filter(post => post.tags.forEach((item)=> {
+        if(item.toLowerCase().includes(searchInput.value.toLowerCase())){
+            return post
+        }
+    }));
+    filterTitle.forEach(item => {
+        filteredArray.push(item)
+    })
+    filterBody.forEach(item => {
+        filteredArray.push(item)
+    })
+    filterTag.forEach(item => {
+        filteredArray.push(item)
+    })
+    const uniqueArray = filteredArray.filter((item, index, self) => {
+        return index === self.findIndex((t) => (
+            t.id === item.id
+        ));
+    });
+    let slicedData = uniqueArray.slice(startIndex, startIndex + postsPerPage);
+    buttonMorePosts.style.display = uniqueArray.length > startIndex + postsPerPage ? "flex" : "none";
+    buttonMorePosts.addEventListener("click", () => {
+        let slicedData = uniqueArray.slice(startIndex, startIndex + postsPerPage);
+        startIndex += postsPerPage;
+        slicedData.forEach((item) => {
+            root.append(renderCard(item));
+        });
+        buttonMorePosts.style.display = uniqueArray.length > startIndex + postsPerPage ? "flex" : "none";
+    });
+    if(uniqueArray.length === 0){
+        root.innerHTML = '<h2 class="no-results">No results found</h2>'
+    } else {
+        slicedData.forEach(item => {
+            root.append(renderCard(item))
+        });
+    }
+}
