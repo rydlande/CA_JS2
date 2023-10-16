f2faa01345f1845fb9c0afe05a6a0ffa2a4fb770
rydlande <rydlande@hotmail.com>
2023-10-16 18:57:39 +0200

    fixed createpost

--- js/modules/createPost.mjs
+++ js/modules/createPost.mjs
@@ -1,10 +1,11 @@
 const urlPosts = "https://api.noroff.dev/api/v1/social/posts/";
-const body = document.getElementById("inputBody").value;
-const media = document.getElementById("inputMedia").value;
-const tags = document.getElementById("inputTags").value;
 
 export default async function createPost() {
   const title = document.getElementById("inputTitle").value;
+  const body = document.getElementById("inputBody").value;
+  const media = document.getElementById("inputMedia").value;
+  const tags = document.getElementById("inputTags").value;
+
   const newPost = {
     title: title,
     body: body,
